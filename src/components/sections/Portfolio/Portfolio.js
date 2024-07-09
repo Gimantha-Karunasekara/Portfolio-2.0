3f83002e8302f1b070f3ac9633e960ab44a8b5f4
Gimantha karunasekara <gimanthakarunasekara@gmail.com>
2024-07-09 12:56:08 +0530

    Portfolio update

--- src/components/sections/Portfolio/Portfolio.js
+++ src/components/sections/Portfolio/Portfolio.js
@@ -11,49 +11,57 @@ import myList_img from '../../assets/img/portfolio/mylist.svg'
 import ccet_img from '../../assets/img/portfolio/ccet.svg';
 import qr_ticket_page from '../../assets/img/portfolio/qrTicketPage.png';
 import weather_app_img from '../../assets/img/portfolio/weather.jpg'
+import curtin_assist_img from '../../assets/img/portfolio/curtinAssist.png';
 
 import SlideShow from '../../common/SlideShow';
 
 const portfolio_data = [
   {
     key: "001",
+    title: "CurtinAssist",
+    desc: "CurtinAssist is a centralized communication platform for university students and staff.",
+    img: curtin_assist_img,
+    visit: "https://www.linkedin.com/posts/vinukatukorala_curtinassist-studentsupportsystem-capstonecomputingproject-activity-7199472259936210944-jw6B?utm_source=share&utm_medium=member_desktop"
+  },
+  {
+    key: "002",
     title: "Ticketing System (Frontend)",
     desc: "Frontend of Ticketing system made for Curtin Colombo Events Team. Book tickets, scan QR, admin dashboard and more.",
     img: ccet_img,
     visit: "https://ccet-ticketing.netlify.app/"
   },
   {
-    key: "002",
+    key: "003",
     title: "MyList",
     desc: "Simple to-do list app to keep track of your tasks. Save your tasks with ease on your own user account. Created using MERN stack.",
     img: myList_img,
     github: "https://github.com/Gimantha-Karunasekara/MyList",
     visit: "https://gimantha-my-list.netlify.app/"
-    
+
   },
   {
-    key: "003",
+    key: "004",
     title: "React Native Weather App",
     desc: "React native weather app with animated backgrounds and current location weather.",
     img: weather_app_img,
     github: "https://github.com/Gimantha-Karunasekara/RN-WeatherApp"
   },
   {
-    key: "004",
+    key: "005",
     title: "Foodies",
     desc: "Java based food ordering Android application which is feature-rich and adaptable to any device form factor.",
     img: foodies_img,
     github: "https://github.com/Gimantha-Karunasekara/Foodies"
   },
   {
-    key: "005",
+    key: "006",
     title: "Animated QR Ticket Page",
     desc: "Animated QR ticket page made using pure HTML, CSS and Javascript.",
     img: qr_ticket_page,
     github: "https://github.com/Gimantha-Karunasekara/QR-Ticket-Page"
   },
   {
-    key: "006",
+    key: "007",
     title: "Portfolio website",
     desc: " Responsive personal portfolio website made using HTML, CSS and Javascript.",
     img: portfolio_img,
@@ -69,7 +77,7 @@ const Portfolio = () => {
       <span className="section__subtitle">My recent work</span>
 
       <div className='portfolio__container'>
-        <SlideShow data={portfolio_data}/>
+        <SlideShow data={portfolio_data} />
       </div>
     </section>
   )
